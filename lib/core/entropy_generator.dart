393a4216b402aeea70bc2e2fa8a7baa2b1a9eca4
Junsung Choi <choiexe1@gmail.com>
2026-02-07 11:10:43 +0900

    bit size

--- lib/core/entropy_generator.dart
+++ lib/core/entropy_generator.dart
@@ -1,9 +1,12 @@
 import 'dart:math';
 
+enum EntropySize { bits128, bits256 }
+
 class EntropyGenerator {
   final Random _random = Random.secure();
 
-  Future<List<int>> generate(int length) async {
+  Future<List<int>> generate(EntropySize size) async {
+    int length = size == EntropySize.bits128 ? 16 : 32;
     return List.generate(length, (_) => _random.nextInt(256));
   }
 }
